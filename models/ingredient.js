ad25c25a5240821e1cf2f30c63390aa4aa1c00f9
boc4life <lheandrew@gmail.com>
2019-03-05 15:51:13 -0500

    added ingredient price column

--- models/ingredient.js
+++ models/ingredient.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const Ingredient = sequelize.define('Ingredient', {
     name: DataTypes.STRING,
-    stock_quantity: DataTypes.INTEGER
+    stock_quantity: DataTypes.INTEGER,
+    price: DataTypes.INTEGER
   }, {});
   Ingredient.associate = function(models) {
     Ingredient.belongsToMany(models.Pizza, {through: "PizzaToppings"})
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
