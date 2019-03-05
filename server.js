585493100354c26ae519743d4bf113e346676190
Stepan <stepos36@gmail.com>
2019-03-05 13:35:03 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
