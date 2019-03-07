17a479b8d9a169f0b8756a127d99d1ea5509dc81
boc4life <lheandrew@gmail.com>
2019-03-06 21:48:18 -0500

    day of week report

--- routes/reports-html-routes.js
+++ routes/reports-html-routes.js
@@ -3,5 +3,9 @@ let path = require("path")
 module.exports = function(app) {
     app.get("/toppingreport", function(req, res){
         res.sendFile(path.join(__dirname + "/../public/html/reports", "toppingReport.html"));
+    });
+
+    app.get("/dayofweek", function(req, res){
+        res.sendFile(path.join(__dirname + "/../public/html/reports", "dayofweek.html"));
     })
 }
