067f844945f2d6db1c840bf30f6673e4caa5a496
boc4life <lheandrew@gmail.com>
2019-03-08 18:35:59 -0500

    temporderlogic added for backend work

--- seeders/20190305220028-ingredients.js
+++ seeders/20190305220028-ingredients.js
@@ -7,7 +7,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
+      return queryInterface.bulkInsert('People', [{ 
         name: 'John Doe',
         isBetaMember: false
       }], {});
