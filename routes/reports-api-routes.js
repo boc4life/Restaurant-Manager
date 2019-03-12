a3d5a9cf37d0d0cf2d7b9fb9a201b81bf5a8fa57
boc4life <lheandrew@gmail.com>
2019-03-11 23:31:52 -0400

    topping report date filter coding completed

--- routes/reports-api-routes.js
+++ routes/reports-api-routes.js
@@ -21,6 +21,34 @@ app.get("/api/toppings", function(req, res){
     })
 })
 
+app.post("/api/toppings", function(req, res){
+    let startDate = parseStart(req.body.startDate);
+    let endDate = parseEnd(req.body.endDate);
+    db.PizzaToppings.findAll({
+        attributes: ["ingredient_id"],
+        where: {
+            created_at: {
+                $between: [startDate, endDate]
+            }
+        }
+        }).then(function(data){
+        let arr = [];
+        for (let i = 0; i < data.length; i++){
+        arr.push(data[i].dataValues.ingredient_id);
+            if (i == (data.length - 1)) {
+                // Create key for each ingredient being logged. These key/value pairs must line up with the labels section of the chart in toppingreport.js
+                var counts = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0};
+                for (var k = 0; k < arr.length; k++) {
+                var num = arr[k];
+                counts[num] = counts[num] ? counts[num] + 1 : 1;
+                }
+                let responseArr = Object.values(counts)
+                res.json(responseArr)
+            }
+        }
+    })
+})
+
 app.get("/api/dayofweek", function(req, res){
     db.Order.findAll({
         attributes: [[db.Order.sequelize.fn("dayofweek", db.Order.sequelize.col("created_at")), "dayofweek"], "subtotal"]
