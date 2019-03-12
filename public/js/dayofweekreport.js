89f943dc16d3506327de53179674662c216af647
boc4life <lheandrew@gmail.com>
2019-03-11 21:02:01 -0400

    completed re-render of day of week chart, must repeat front and back end logic for other date filtered reports

--- public/js/dayofweekreport.js
+++ public/js/dayofweekreport.js
@@ -65,6 +65,44 @@ $(document).on("click", "#loadReport", function(event){
             endDate: endDate
         }
     }).then(function(data){
-        console.log(data)
+        graphData = data;
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                datasets: [{
+                    label: 'Sales',
+                    data: graphData,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(40, 91, 200, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(40, 91, 200, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
     })
 })
